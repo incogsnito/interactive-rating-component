c1ea63c188c0ddd423e3123be3e3636e7fab3de0
Samad <Samad.o@outlook.com>
2025-11-29 15:43:05 -0500

    playing around with javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+// Single line comment In-line
+/* Multi
+line 
+comment */
+
+/* Data types:
+Undefined, no value. NULL, nothing. Boolean, true or false. String, text. Symbol. Number. Object, key value pairs. */
+
+var myName = "Samad";
+
+myName = 8
+
+console.log(myName)
