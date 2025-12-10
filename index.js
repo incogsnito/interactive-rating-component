d014ee53b48a8cd6e349ff6963b81ccd60b546d2
Samad <Samad.o@outlook.com>
2025-12-10 06:34:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,22 @@ const submit = document.querySelector("a");
 
 const box = document.querySelector(".box");
 
-submit.addEventListener('click', ()=> {    //Makes white box display for now.
+submit.addEventListener("click", () => {
+  //Makes white box display for now.
   ratingComponent.style.display = "none";
-  box.style.display = "block";   
+  box.style.display = "block";
 });
 
+rating.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.style.background = "white";
+    item.style.color = "hsl(216, 12%, 8%)";
+  });
+});
+
+const myItems = document.querySelectorAll(".my-item");
+
+myItems.forEach((item) => {
+  item.textContent += " - Modified!";
+  item.style.color = "red";
+});
