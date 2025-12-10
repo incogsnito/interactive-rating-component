b15bb51b1f9bd8ddeb4a80e7fd8f8dbddf7352d6
Samad <Samad.o@outlook.com>
2025-12-09 19:15:08 -0500

    removed unused js code, working on fixing the js code as well

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-// Single line comment In-line
-/* Multi
-line 
-comment */
+const ratingComponent = document.querySelector(".rating__component");
 
-/* Data types:
-Undefined, no value. NULL, nothing. Boolean, true or false. String, text. Symbol. Number. Object, key value pairs. */
+const rating = document.querySelectorAll(".rating__btn");
 
-var myName = "Samad";
-
-var a;
-console.log(a);
-
-myName = 8;
-
-console.log(myName);
-
-var myStr = "I am a \" double \" ";
-
-console.log(myStr);
-
-
-var firstName = "Samad";
-var firstLetterOfName = firstName[0];
-
-console.log(firstLetterOfName);
