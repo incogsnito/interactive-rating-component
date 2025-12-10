e816b8301640eecf0f3f75c138c5535ce84e08f8
Samad <Samad.o@outlook.com>
2025-12-09 19:50:35 -0500

    Added pop up screen when submit is clicked. Last edits are styling, transition and conditional for .rating__btn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const ratingComponent = document.querySelector(".rating__component");
 
-const rating = document.querySelectorAll(".rating__btn");
+const rating = document.querySelectorAll("li");
+
+const submit = document.querySelector("a");
+
+const box = document.querySelector(".box");
+
+submit.addEventListener('click', ()=> {    //Makes white box display for now.
+  ratingComponent.style.display = "none";
+  box.style.display = "block";   
+});
 
