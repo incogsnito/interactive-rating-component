e106d0126a887cc7f4a5177852fe1dab2d540730
Samad <Samad.o@outlook.com>
2025-12-22 14:09:01 -0500

    Project complete

--- index.js
+++ index.js
@@ -6,20 +6,39 @@ const submit = document.querySelector("button");
 
 const box = document.querySelector(".thank-you");
 
+const afterRate = document.querySelector(".rate");
 
-submit.addEventListener("click", () => {
-  ratingComponent.style.display = "none";
-  box.style.display = "flex";
-  box.style.flexDirection = "column";
-  box.style.gap = "3em";
+box.classList.add("invisible");
 
-});
+let selectedRating = null;
+
+//Changes state
 
 rating.forEach((item) => {
-  item.addEventListener("click", () => {
-    item.style.background = "hsl(25, 97%, 53%)";
+  item.addEventListener("click", (e) => {
+    // Grabs the id(data);
+    const target = e.target;
+    const targetId = e.target.id;
+
+    // cycles through, removes active class
+    rating.forEach((num) => {
+      num.classList.remove("active");
+    });
+
+    // Brings back one active class
+    item.classList.add("active");
+
+    // Gives var a new value
+    selectedRating = targetId;
   });
 });
 
-
-
+submit.addEventListener("click", () => {
+  if (selectedRating) {
+    ratingComponent.classList.add("invisible");
+    box.classList.remove("invisible") && add("thank-you");
+    afterRate.textContent = "You selected " + selectedRating + " out of 5";
+  } else {
+    alert("Please select a rating first");
+  }
+});
